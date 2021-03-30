e4657b89f27c839b3e8a9538a1e1cbeaa59892df
AnaximanderMKD <marjan.lazarov@intertec.io>
2021-03-30 13:59:34 +0200

    index page

---
+++ index.js
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Liverpool Fan Page</title>
+</head>
+<body>
+    <p>This is a Liverpool Fan site!</p>
+    
+    
+</body> 
+
+
+
+
+
+<footer>
+    <p>Contacts</p>
+</footer>
+
+</html>
